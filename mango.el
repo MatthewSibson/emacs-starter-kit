4de9bb5c00792f555bec1f38cbf39ca2adf1b8a9
Matthew Sibson <matthew@matthewsibson.com>
2011-05-16 09:57:58 -0400

    Commiting mango changes

--- mango.el
+++ mango.el
@@ -1,5 +1,8 @@
 ;; Pretty color theme
-(color-theme-twilight)
+;;(color-theme-twilight)
+
+;; Start server for emacsclient
+(server-start)
 
 ;; Go mode
 (add-to-list 'load-path "~/.golang/misc/emacs" t)
@@ -11,3 +14,20 @@
 (setq org-mobile-inbox-for-pull "~/.org/flagged.org")
 ;; Set to <your Dropbox root directory>/MobileOrg.
 (setq org-mobile-directory "~/Dropbox/MobileOrg")
+;; Log when done
+(setq org-log-done t)
+;; Agenda files
+(setq org-agenda-files (list "~/.org/work.org" "~/.org/home.org"))
+
+;; Make sure I'm really sure I want to close emacs
+(setq confirm-kill-emacs 'y-or-n-p)
+
+;; Slime
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "/Users/matthew/.homebrew/bin/clisp")
+
+;; Erlang
+(add-to-list 'load-path (expand-file-name "~/.homebrew/lib/erlang/lib/tools-2.6.6.3/emacs"))
+(setq erlang-root-dir (expand-file-name "~/.homebrew/lib/erlang"))
+(add-to-list 'exec-path (expand-file-name "~/.homebrew/lib/erlang/bin"))
+(require 'erlang-start)
